662e0d36409f427251b26d25ae839576f6561214
AlbertAndrea <92324558+AlbertAndrea@users.noreply.github.com>
2022-06-27 11:54:07 +0200

    docker ok

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,5 @@
 const app = require('./server');
-const port = process.env.post || 3000;
+const port = process.env.port || 3000;
 
 app.listen(port, () => {
     console.log(`App is listening at http://localhost:${port}`);
